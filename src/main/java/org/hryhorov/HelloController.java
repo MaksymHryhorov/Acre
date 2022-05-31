3e46833a714b1b8ef6c8a2a105f15feca5a58407
Maxim <maximgrigorovua@gmail.com>
2022-05-31 14:29:26 +0300

    first commit

---
+++ src/main/java/org/hryhorov/HelloController.java
@@ -0,0 +1,13 @@
+package org.hryhorov;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
