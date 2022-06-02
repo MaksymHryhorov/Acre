70daaf92cf3c81eb8b9d9a65dcd839d96b457bde
Maxim <maximgrigorovua@gmail.com>
2022-06-02 17:09:09 +0300

    Requests with parameters

--- src/main/java/org/hryhorov/Controllers/FirstController.java
+++ src/main/java/org/hryhorov/Controllers/FirstController.java
@@ -3,18 +3,30 @@ package org.hryhorov.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hi")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello " + name + " " + surname);
+
         return "first/hi";
     }
 
     @GetMapping("goodbye")
-    public String goodBye() {
+    public String goodBye(@RequestParam(value = "name", required = false) String name,
+                          @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello " + name + " " + surname);
+
         return "first/goodbye";
     }
 }
