ae439b18aab64a22214375dd2ff01663ed342b99
Maxim <maximgrigorovua@gmail.com>
2022-06-04 13:35:56 +0300

    Data transfer from controller to view

--- src/main/java/org/hryhorov/Controllers/FirstController.java
+++ src/main/java/org/hryhorov/Controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.hryhorov.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,10 +24,43 @@ public class FirstController {
 
     @GetMapping("goodbye")
     public String goodBye(@RequestParam(value = "name", required = false) String name,
-                          @RequestParam(value = "surname", required = false) String surname) {
+                          @RequestParam(value = "surname", required = false) String surname,
+                          Model model) {
 
-        System.out.println("Hello " + name + " " + surname);
+        //System.out.println("Hello " + name + " " + surname);
+        model.addAttribute("message","Hello " + name + " " + surname);
 
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model) {
+
+        double result;
+
+        switch (action) {
+            case "multi":
+                result = a * b;
+                break;
+            case "div":
+                result = a / (double) b;
+                break;
+            case "sub":
+                result = a - b;
+                break;
+            case "add":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
